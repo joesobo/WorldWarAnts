6317454d7362c280fcec4abbecfa5e3a7d3cd1da
joesobo <soboleskijoe@gmail.com>
2021-06-03 18:47:44 -0700

    trying and failing to make ant walk

--- Assets/Scripts/Player/LegMovement.cs
+++ Assets/Scripts/Player/LegMovement.cs
@@ -6,31 +6,46 @@ public class LegMovement : MonoBehaviour {
     public Transform currentTarget;
     public Transform desiredTarget;
     public float speed = 1;
+    public float maxDistance = 0.1f;
+    public float maxTargetDist = 0.3f;
     public AnimationCurve yCurve;
 
-    private float timer;
+    private float timer = 0;
+    private bool moving = false;
+
+    private void Start() {
+        currentTarget.position = FindObjectOfType<PlayerController>().transform.position + desiredTarget.position;
+    }
 
     private void Update() {
         float dist = Vector2.Distance(transform.position, currentTarget.position);
 
+
+        if (dist > maxDistance) {
+            moving = true;
+        }
+        else if (dist == 0) {
+            moving = false;
+        }
+
         // Move Foot
-        if (dist > 0.01f) {
-            timer = 0;
+        if (moving) {
             timer += Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(currentTarget.position.x, currentTarget.position.y + yCurve.Evaluate(timer)), speed * Time.deltaTime);
-        } 
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(desiredTarget.position.x, currentTarget.position.y + yCurve.Evaluate(timer)), speed * Time.deltaTime);
+        }
         // Clamp Foot
         else {
             transform.position = currentTarget.position;
+            timer = 0;
         }
     }
 
     private void FixedUpdate() {
-        transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, speed * Time.deltaTime);
+        // transform.position = Vector2.MoveTowards(transform.position, currentTarget.position, speed * Time.deltaTime);
 
         float dist = Vector2.Distance(currentTarget.position, desiredTarget.position);
 
-        if (dist > 0.3f) {
+        if (dist > maxTargetDist) {
             currentTarget.position = desiredTarget.position;
         }
     }
